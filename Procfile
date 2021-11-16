5a1172f633afdb3b0a56dd330519ad402cc63c23
Owais Quadri <owais.quadri@ontariotechu.net>
2021-11-16 17:22:29 -0500

    update deployment

--- Procfile
+++ Procfile
@@ -1,4 +1,4 @@
 release: python manage.py makemigrations --no-input
 release: python manage.py migrate --no-input
-
+release: python manage.py collectstatic --no-input
 web: gunicorn ValidationAPI.wsgi
--- ValidationAPI/settings.py
+++ ValidationAPI/settings.py
@@ -24,12 +24,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-9w_evbrp#=%o32v)!b@!#%0f(-$bjvsijhqt$$f%u&sj#ma^i9'
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
@@ -47,6 +47,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     #'django.middleware.csrf.CsrfViewMiddleware',
