d23f45b2c8b2c58e7f015414e8cd6de4fa870edd
Owais Quadri <owais.quadri@ontariotechu.net>
2021-11-16 21:48:25 -0500

    debugging

--- face_api/faceDetect/detect.py
+++ face_api/faceDetect/detect.py
@@ -5,6 +5,7 @@ import os
 from os import path
 from ..models import Face
 from django.conf import settings
+import socket
 # face_recognition --tolderance .40 ./known ./input
 class Detect:
     def __init__(self):
@@ -15,7 +16,7 @@ class Detect:
         recognized = ""
         # load image to save
         print(settings.MEDIA_ROOT,settings.MEDIA_URL)
-        head=os.path.join(settings.MEDIA_ROOT,settings.MEDIA_URL)
+        head=os.path.join(socket. gethostname(),settings.MEDIA_ROOT,settings.MEDIA_URL)
         # if not settings.IS_WIN:
         #     imagesDir=imagesDir.replace("\\","/")
         # head=os.path.dirname(settings.BASE_DIR)+ imagesDir# fixes when system changes
