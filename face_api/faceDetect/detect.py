c4f5c46e686ad048fc3ca7bd5fb145b8bc4032f4
Owais Quadri <owais.quadri@ontariotechu.net>
2021-11-17 13:50:59 -0500

    heroku testing

--- face_api/faceDetect/detect.py
+++ face_api/faceDetect/detect.py
@@ -21,8 +21,6 @@ class Detect:
         recognized = ""
         # load image to save
         FMR=settings.MEDIA_ROOT
-        # if not settings.IS_WIN:
-        #     imagesDir=imagesDir.replace("\\","/")
         # head=os.path.dirname(settings.BASE_DIR)+ imagesDir# fixes when system changes
         # get faces of random pic input
         input_image_path=FMR / str(Face.objects.last().face)
@@ -32,45 +30,23 @@ class Detect:
         input_locations = fr.face_locations(input_image)
         numOfInputs = len(input_locations)
         count = 0
-        for input in input_locations:
-            count += 1
-            top, right, bottom, left = input
-            face_input = input_image[top:bottom, left:right]
-            pil_image = Image.fromarray(face_input)
-            input_path=FMR / 'media' / 'images' / 'input' 
-            inputcount_path=input_path / ('input'+str(count)+'.png')
-            with open(inputcount_path, mode='rb') as input_file:
-                pil_image.save(input_file,format='png',)
+        input_encodings = fr.face_encodings(input_image,known_face_locations=input_locations)
         self.delete_unknowns()
         # get face encoding of knowns
         print ("Known users:")
         for f in known_faces:
-            
             name_of_known = str(f.name)
             print(name_of_known)
             known_path=FMR / str(f.face)
             image_of_known = fr.load_image_file(known_path)
             known_face_encoding = fr.face_encodings(image_of_known)[0]
-
-            # compare current known against inputs
-            inputLocation = os.listdir(input_path)
             
-            for i in inputLocation:
-                i_path=input_path / str(i)
-                
-                img = fr.load_image_file(i_path)
-                im=Image.open(i_path)
-                
-                w,h=im.size
-                kfl=[(0, w, h, 0)]
-                im.close()
-                face_encoding = fr.face_encodings(img,known_face_locations=kfl)
-                
-                match = fr.compare_faces(known_face_encoding, face_encoding)[0]
+            for i in input_encodings:
+                match = fr.compare_faces(known_face_encoding, i)[0]
                 
                 if match == True:
                     print("recognized!")
-                    output+= name_of_known.replace("_"," ")+","
+                    output+= name_of_known+","
         try:
             if output[-1]==",":
                 output=output[:-1]
