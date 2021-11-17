d3a415a0e04c16b7b995a1aca281b41e67ef4b0b
Owais Quadri <owais.quadri@ontariotechu.net>
2021-11-17 01:25:49 -0500

    heroku testing

--- face_api/faceDetect/detect.py
+++ face_api/faceDetect/detect.py
@@ -53,7 +53,7 @@ class Detect:
             
             name_of_known = str(f.name)
             print(name_of_known)
-            known_path=FMR / str(f.face)
+            known_path=FM / str(f.face)
             image_of_known = fr.load_image_file(known_path)
             known_face_encoding = fr.face_encodings(image_of_known)[0]
 
