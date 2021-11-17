649ff277768483a65c136d2abd62c993f18de2d2
Owais Quadri <owais.quadri@ontariotechu.net>
2021-11-17 17:40:16 -0500

    authentication

--- ValidationAPI/settings.py
+++ ValidationAPI/settings.py
@@ -45,7 +45,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'face_api',
-    
+    'rest_framework.authtoken'
 ]
 
 REST_FRAMEWORK={
--- ValidationAPI/urls.py
+++ ValidationAPI/urls.py
@@ -17,8 +17,9 @@ from django.contrib import admin
 from django.urls import path,include
 from django.conf.urls.static import static
 from django.conf import settings
-
+from rest_framework.authtoken import views
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('face_api.urls'))
+    path('', include('face_api.urls')),
+    path('auth/',views.obtain_auth_token, name='api-token-auth')
 ]+ static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
